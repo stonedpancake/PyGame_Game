ea300d5ba7bd73c0e00c3fb05d4ab2390caa75cf
Вячеслав <slava2005qwe@gmail.com>
2020-12-09 19:14:53 +0300

    7 commit

--- Test Applications/Test_App_2.py
+++ Test Applications/Test_App_2.py
@@ -1,68 +1,37 @@
-import pygame
+import pygame as pg
+import sys
 
+pg.init()
+sc = pg.display.set_mode((400, 300))
 
-class Board:
-    # создание поля
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for i in range(height)]
-        # значения по умолчанию
-        self.left = 50
-        self.top = 50
-        self.cell_size = 50
+pg.mixer.music.load('Song_1.mp3')
+pg.mixer.music.play()
 
-    # настройка внешнего вида
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
+'''sound1 = pg.mixer.Sound('boom.wav')
+sound2 = pg.mixer.Sound('one.ogg')'''
 
-    def render(self, screen):
-        for y in range(self.height):
-            for x in range(self.width):
-                if self.board[y][x] == 0:
-                    bord_width = 1
-                else:
-                    bord_width = 0
-                pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
-                    x * self.cell_size + self.left,
-                    y * self.cell_size + self.top,
-                    self.cell_size, self.cell_size), bord_width)
+while 1:
+    for i in pg.event.get():
+        if i.type == pg.QUIT:
+            sys.exit()
 
-    def get_cell(self, mouse_pos):
-        cell_x = (mouse_pos[0] - self.left) // self.cell_size
-        cell_y = (mouse_pos[1] - self.top) // self.cell_size
-        if cell_x < 0 or cell_x > self.width or cell_y < 0 or cell_y > self.height:
-            return None
-        return cell_x, cell_y
+        elif i.type == pg.KEYUP:
+            if i.key == pg.K_1:
+                pg.mixer.music.pause()
+                # pygame.mixer.music.stop()
+            elif i.key == pg.K_2:
+                pg.mixer.music.unpause()
+                # pygame.mixer.music.play()
+                pg.mixer.music.set_volume(0.5)
+            elif i.key == pg.K_3:
+                pg.mixer.music.unpause()
+                # pygame.mixer.music.play()
+                pg.mixer.music.set_volume(1)
 
-    def on_click(self, cell_coords):
-        if cell_coords is not None:
-            x, y = cell_coords
-            self.board[y][x] = (self.board[y][x] + 1) % 2
+        elif i.type == pg.MOUSEBUTTONUP:
+            if i.button == 1:
+                sound1.play()
+            elif i.button == 3:
+                sound2.play()
 
-    def get_click(self, mouse_pos):
-        cell = self.get_cell(mouse_pos)
-        self.on_click(cell)
-
-
-pygame.init()
-size = 500, 500
-screen = pygame.display.set_mode(size)
-# поле 5 на 7
-board = Board(5, 7)
-# board = Board(4, 3)
-# board.set_view(100, 100, 50)
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            board.get_click(event.pos)
-    screen.fill((0, 0, 0))
-    board.render(screen)
-    pygame.display.flip()
-pygame.quit()
+    pg.time.delay(20)
